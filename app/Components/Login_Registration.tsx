532396469a01f49ea220e4032a36f1055e0a919b
Duba Bence <duba.bence22@gmail.com>
2024-09-23 12:45:58 +0200

    push

--- app/Components/Login_Registration.tsx
+++ app/Components/Login_Registration.tsx
@@ -2,12 +2,52 @@ import React from "react";
 import { Input, Spacer, Button } from "@nextui-org/react";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
+
 function Login() {
   const [Swap, setSwap] = useState(true);
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    passwordSecond:''
+  })
+  const [valid, setValid] = useState(false)
+
 
   const SwapLabel = () => {
     setSwap(!Swap);
+    setData({
+      ...data,
+      email: '',
+      password: '',
+      passwordSecond:''
+    })
+    setValid(false)
   };
+
+  const handleSubmit = (event:  React.FormEvent<HTMLFormElement>) =>{
+    const {email, password, passwordSecond} = data;
+    event.preventDefault()
+
+    console.log(data)
+    if(!email || !password || !passwordSecond)
+    {
+      toast.error("Fill the inputs")
+      setValid(true)
+    }
+    else if(password !== passwordSecond)
+    {
+      toast.error("The passwords are not matching")
+      setValid(true)
+    }
+    else
+      toast.success("The registration was successfull")
+    
+  }
 
   return (
     <>
@@ -15,7 +55,7 @@ function Login() {
         <div className="overflow-hidden">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }} 
+          animate={{ opacity: 2, scale: 1 }} 
           className="flex flex-col items-center justify-center min-h-screen text-white text-center"
         >
           <div className="bg-transparent rounded-lg border-2 p-6 shadow-2xl">
@@ -31,7 +71,7 @@ function Login() {
                 label="Email"
                 size="sm"
                 labelPlacement="outside"
-                className="md:w-64 lg:w-80 xl:w-96"
+                className="md:w-64 lg:w-80 xl:w-96 placeholder:text-xl"
               />
               <Input
                 type="email"
@@ -54,7 +94,7 @@ function Login() {
         </motion.div>
         </div>
       ) : (
-        
+        <form onSubmit={handleSubmit}>
         <motion.div
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }} 
@@ -72,6 +112,10 @@ function Login() {
                 variant="flat"
                 label="Email"
                 size="sm"
+                value={data.email}
+                
+                onChange={(e) => setData({...data, email: e.target.value})}
+                isInvalid={valid}
                 labelPlacement="outside"
                 className="md:w-64 lg:w-80 xl:w-96"
               />
@@ -80,19 +124,26 @@ function Login() {
                 variant="flat"
                 label="Password"
                 size="sm"
+                value={data.password}
+                onChange={(e) => setData({...data, password: e.target.value})}
+                isInvalid={valid}
                 labelPlacement="outside"
                 className="md:w-64 lg:w-80 xl:w-96"
               />
               <Input
                 type="password"
                 variant="flat"
+                value={data.passwordSecond}
+                onChange={(e) => setData({...data, passwordSecond: e.target.value})}
                 label="Password Confirm"
                 size="sm"
+                isInvalid={valid}
                 labelPlacement="outside"
                 className="md:w-64 lg:w-80 xl:w-96"
               />
               <Button
-                radius="lg"
+              type="submit"
+               radius="lg"
                 variant="solid"
                 className="bg-gradient-to-tr from-purple-500 to-purple-700 text-white font-bold w-full mt-5"
               >
@@ -102,8 +153,20 @@ function Login() {
             </div>
           </div>
         </motion.div>
-
+        </form>
       )}
+      <ToastContainer
+position="bottom-center"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import Login from './Components/Login_Registration'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import  Navbar  from './Components/Navbar';
 
 export default function Home() {
   return (
+    <>
+      <Navbar/>
       <Login/>  
+      </>
   );
 }
+`x`
